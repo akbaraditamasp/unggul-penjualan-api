1649967f921e02e3cf589d92805313b92a3c5a0e
akbaraditamasp <akbaraditamasp@gmail.com>
2025-01-23 22:40:14 +0700

    initial commit

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        [
+            "email" => $email,
+            "password" => $password
+        ] = $request->validate([
+            "email" => 'required|email:rfc,dns',
+            "password" => 'required'
+        ]);
+
+        $user = User::where('users.email', $email)->first();
+
+        if (!$user || !Hash::check($password, $user->password)) {
+            return response()->json([
+                "error" => "Invalid email or password"
+            ], 401);
+        }
+
+        return response()->json([
+            ...$user->toArray(),
+            "token" => $user->createToken("APP Token")->plainTextToken
+        ]);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json($request->user()->toArray(), 200);
+    }
+}
